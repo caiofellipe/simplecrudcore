8119b79a1134078582722145621b00b07589b97f
Caio <caiofellipe80@gmail.com>
2023-09-14 14:40:21 -0300

    feat(initial): initial commit

---
+++ src/test/java/br/com/simplecrudcore/SimplecrudcoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.simplecrudcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplecrudcoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
